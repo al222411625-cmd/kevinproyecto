1c930035034abe4127c537ed726a8fb5666e1ddc
Kevin Arias <al222411625@gmail.com>
2026-05-26 15:12:15 -0600

    fix smtp render

--- mailer.js
+++ mailer.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const sendMail = async (to, subject, html) => {
@@ -16,7 +21,9 @@ const sendMail = async (to, subject, html) => {
       subject,
       html,
     });
+
     console.log("Correo enviado a:", to);
+
   } catch (err) {
     console.error("Error correo:", err);
   }
