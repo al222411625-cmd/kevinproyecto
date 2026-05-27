a7a25d5c5e53bc2ee1c4a3176018827ecc5b4ad6
Kevin Arias <al222411625@gmail.com>
2026-05-27 17:28:26 -0600

    fix gmail timeout

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 transporter.verify((error, success) => {
