6e1dd533c73d7a680e060d2f1de8f1f1100c0f17
Kevin Arias <al222411625@gmail.com>
2026-06-03 08:58:06 -0600

    debug correo

--- mailer.js
+++ mailer.js
@@ -1,32 +1,26 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  service: 'gmail',
   port: 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   },
   tls: {
     rejectUnauthorized: false
   }
 });
 
-const sendMail = async (to, subject, html) => {
-  try {
-    await transporter.sendMail({
-      from: `"ITrack" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-    });
-
-    console.log("Correo enviado a:", to);
-
-  } catch (err) {
-    console.error("Error correo:", err);
-  }
-};
+async function sendMail(to, subject, html) {
+  return transporter.sendMail({
+    from: `"ITrack Soporte" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html
+  });
+}
 
 module.exports = sendMail;
+
